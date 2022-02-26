e707b722862619705469f7dcc35372158f9ce989
Steve Franchak <stevefranchak@gmail.com>
2022-02-26 10:32:38 -0500

    Update clap to latest

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::rc::Rc;
 
-use clap::{crate_authors, crate_description, crate_version, Clap};
+use clap::{Parser, Subcommand};
 use env_logger::{Builder, Env};
 use log::LevelFilter;
 
@@ -13,14 +13,14 @@ mod archive;
 mod emoji;
 mod slack;
 
-#[derive(Clap)]
-#[clap(version = crate_version!(), author = crate_authors!(), about = crate_description!())]
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
 struct Opts {
     /// Slack workspace subdomain (e.g. if your Slack is at myorg.slack.com, enter "myorg")
     #[clap(name = "SLACK WORKSPACE")]
     workspace: String,
     /// Path to directory to either download emojis to or upload emojis from. The `download` subcommand will attempt
-    /// to create a directory at the provided path if it does not exist, whereas the `upload` subcommand expects
+    /// to create a directory at the provided path if one does not exist. The `upload` subcommand expects
     /// that the provided path is an existing directory containing a well-formed 'metadata.ndjson' and emoji files.
     #[clap(name = "TARGET DIRECTORY")]
     target_directory: String,
@@ -28,6 +28,8 @@ struct Opts {
     /// The token can be manually acquired by inspecting the payload for a request, such as POST /api/emoji.adminList,
     /// via a browser's network dev tools when accessing a Slack workspace's customize/emoji page.
     /// The token generally starts with "xox".
+    ///
+    /// It is STRONGLY advised to provide this argument via the environment variable SLACK_TOKEN
     #[clap(
         name = "slack token",
         short = 't',
@@ -38,16 +40,16 @@ struct Opts {
     token: String,
     /// Sets the log level based on occurrences. The default log level includes ERROR and WARN messages. One occurrence
     /// includes INFO messages, two occurrences include DEBUG messages, and three or more occurrences include TRACE
-    /// messages. The log level can also be set via the environment variable "SLACK_EMOJI_LOG_LEVEL". This argument, if
+    /// messages. The log level can also be set via the environment variable SLACK_EMOJI_LOG_LEVEL. This argument, if
     /// provided, takes precedence over the aforementioned environment variable.
     #[clap(short, parse(from_occurrences))]
     verbose: u8,
     #[clap(subcommand)]
-    subcmd: SubCommand,
+    subcmd: SubCommandKind,
 }
 
-#[derive(Clap)]
-enum SubCommand {
+#[derive(Subcommand)]
+enum SubCommandKind {
     /// Downloads emojis from SLACK WORKSPACE to TARGET DIRECTORY
     Download,
     /// Uploads emojis to SLACK WORKSPACE from TARGET DIRECTORY
@@ -83,7 +85,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     setup_logging(opts.verbose);
     let slack_client = Rc::new(SlackClient::from(&opts));
     match opts.subcmd {
-        SubCommand::Download => download(slack_client, &opts.target_directory).await,
-        SubCommand::Upload => upload(slack_client, &opts.target_directory).await,
+        SubCommandKind::Download => download(slack_client, &opts.target_directory).await,
+        SubCommandKind::Upload => upload(slack_client, &opts.target_directory).await,
     }
 }
